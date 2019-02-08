1b48f55e88c72e0e165eb79e5d0d17779e3c8767
Charlotte Brand <git>
2019-02-08 10:49:08 +0000

    instructions & questionnaire
    Just pushing the instruction and questionnaire changes before I forget

--- static/scripts/questionnaire.js
+++ static/scripts/questionnaire.js
@@ -8,9 +8,9 @@ $(document).ready(function() {
 		bonus = resp.participant.property2;
 		$("#score_info").html(score);
 		if (bonus == "true") {
-     		bonus_eligible = 'and will receive the $20 bonus payment!';
+     		bonus_eligible = '/100 and will receive the $20 bonus payment!';
  		} else {
-     		bonus_eligible = 'but unfortunately you did not score enough points for the bonus payment';
+     		bonus_eligible = '/100 but unfortunately you did not score enough points for the bonus payment';
  		}
  		$("#bonus_info").html(bonus_eligible);
 	})
